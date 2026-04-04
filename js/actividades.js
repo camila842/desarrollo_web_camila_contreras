106bb669a8503f3f2ee428557e3ea19fab35ac1e
Contreras Manriquez Camila Andrea <camila-andrea.contreras-manriquez@student-cs.fr>
2026-04-04 20:30:31 -0300

    [ADD] first activity and functional deploying of its times

--- js/actividades.js
+++ js/actividades.js
@@ -1,35 +1,39 @@
-// const addActivityButton = document.getElementById("add-activity");
-// addActivityButton.addEventListener("click",() => {console.log("hola")});
-// const templateActivity = document.getElementById("activity");
-// const div = document.getElementById("activity-container");
-// // activateButton(addActivityButton,templateActivity,div);
 
-// const templateTimeActivity = document.getElementById("time-selector")
-
-// let cloneTemplate = (template,div) => {
-//     const clon = template.content.cloneNode(true);
-//     div.appendChild(clon);
-// }
-
-// let activateButton = (button,template,div) => {
-//     button.addEventListener("click",() => cloneTemplate(template,div));
-// }
-
-// let weekButtons = (template) => {
-//     const weekDays = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
-
-//     for (let i=0; i<7; i++){
-//         let dayId = "day-";
-//         dayId += weekDays[i];
-//         var dayButton = document.getElementById(dayId);
-//         let divId = weekDays[i] + "-div";
-//         let div = getElementById(divId);
-//         activateButton(dayButton,template,div);
-//     }
-// }
-
-// weekButtons(templateTimeActivity);
 let activitiesRegisterCounter = 0;
+conts activityTemplate = document.getElementsByClassName("activity-container");
+
+const cloneTimes = () => {
+    let timeInput = document.getElementsByClassName("time-selector")[0];
+    const weekDays = ["tuesday","wednesday","thursday","friday","saturday","sunday"];
+    for (let day of weekDays){
+        let cloneInput = timeInput.cloneNode(true);
+        let dayDiv = document.getElementById(`${day}-div`);
+        dayDiv.appendChild(cloneInput);
+    }
+};
+//cloneTimes();
+const addTime = (inputValue,checked) => {
+
+    let dayDiv = document.getElementById(`${inputValue}-div`);
+    if (checked){
+        let timeInput = dayDiv.getElementsByClassName("time-selector")[0];
+        if (timeInput){
+            timeInput.className = "time-selector visible";
+        }
+        else{
+            timeInput = document.getElementsByClassName("time-selector")[0];
+            let newTime = timeInput.cloneNode(true);
+            newTime.className = "time-selector visible";
+            dayDiv.appendChild(newTime);
+        }
+    }
+    else{
+        let timeInput = dayDiv.getElementsByClassName("time-selector visible")[0];
+        if (timeInput) {
+            timeInput.className = "time-selector";
+        }
+    }
+};
 
 const addActivity = (event) => {
     event.preventDefault();
@@ -38,16 +42,45 @@ const addActivity = (event) => {
         let activityForm = document.getElementsByClassName("activities-register")[0];
         activityForm.className = "activities-register visible";
         activitiesRegisterCounter += 1;
+        
     }
     else{
         if(activitiesRegisterCounter <= 2){
             let activityForm = document.getElementsByClassName("activities-register visible")[0];
             let newActivity = activity.cloneNode(true);
+            let daysSelector = newActivity.getElementsByClassName("day-selector");
+            const timesDays = daysSelector.getElementsByName("week-day");
+            for (day in daysSelector.getElementsByClassName("day")){
+                let input = timesDays[day];
+                let dayInput = daysSelector.getElementsByClassName("day")[day];
+                let time = dayInput.getElementsByClassName("time-selector")[0];
+                let timeVisible = input.getElementsByClassName("time-selector visible")[0];
+                if (timeVisible){
+                    timeVisible.className = "time-selector"
+                }
+                input.addEventListener("change",(event) => {
+                    addTime(event.target.value,event.target.checked);
+                });
+            }
             activityForm.appendChild(newActivity);
             activitiesRegisterCounter += 1;
+            
         }
     }
 };
 
 const addActBtn = document.getElementById("add-activity");
 addActBtn.addEventListener("click",addActivity);
+
+const weekDays = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+for (let day of document.getElementsByName("week-day")){
+    //const dayNode = document.getElementById(`day-${day}`);
+    day.addEventListener("change",(event) => {
+        addTime(event.target.value,event.target.checked);
+    });
+}
+
+// const monday = document.getElementById("day-monday");
+// monday.addEventListener("click", (event) => {
+//     addTime(event.target.value);
+// });
